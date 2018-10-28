934112cbb22656a4abae8853e5225e1c5d6bb28f
vad <masking@mail.ru>
2018-10-28 16:23:13 +0300

    port for listen

--- server.js
+++ server.js
@@ -15,5 +15,6 @@ const server = http.createServer(function (request, response) {
     }
 })
 
-server.listen('3000');
-console.log('server started');
+const port = process.env.PORT || 3000;
+server.listen(port);
+console.log('server started on port ', port);
